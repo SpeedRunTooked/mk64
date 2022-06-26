81a6db61a399b2c5b71d1e4838c4b2d1d32d6352
Gustavo Abranches <gabranches@gmail.com>
2022-06-26 00:09:06 -0400

    Fix api url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 const config = {
-    ADD_URL: 'http://localhost:5001/mk64-ad77f/us-central1/addTime',
+    ADD_URL: 'https://us-central1-mk64-ad77f.cloudfunctions.net/addTime',
+    // ADD_URL: 'http://localhost:5001/mk64-ad77f/us-central1/addTime',
     ROOT_URL: 'https://mk64-ad77f-default-rtdb.firebaseio.com/games/mk64',
 };
 
